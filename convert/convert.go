5b6f35147700dfcc4c413057803dfb526251acdb
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2023-09-20 22:39:05 +0000

    begin work on the converter module

---
+++ convert/convert.go
@@ -0,0 +1,34 @@
+package convert
+
+import (
+	"log"
+
+	"github.com/Inalegwu/vsconvert/theme"
+)
+
+const (
+	HelixThemeConfig = 1
+)
+
+type Converter struct {
+	to int
+}
+
+func New(to int) Converter {
+	return Converter{
+		to,
+	}
+}
+
+func (c *Converter) Convert(theme theme.Theme) error {
+	if c.to == HelixThemeConfig {
+		c.toHelixConfig(theme)
+	} else {
+		log.Fatal("Unsupported Theme config provided , If there's one you want to see , create a PR")
+	}
+	return nil
+}
+
+func (c *Converter) toHelixConfig(theme theme.Theme) {
+	log.Printf("Converting %s to Helix Theme Format", theme.Name)
+}
