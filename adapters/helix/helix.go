1239f48ed60c544d1f63b93b6b41ff0b82283712
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2023-09-21 18:04:21 +0000

    making api request to get color code names to create a palette

--- adapters/helix/helix.go
+++ adapters/helix/helix.go
@@ -1,14 +1,63 @@
 package helix
 
-import "github.com/Inalegwu/babel/theme"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Inalegwu/babel/error"
+	"github.com/Inalegwu/babel/theme"
+)
 
 type (
 	HelixThemeConfig struct{}
 	Palette          map[string]string
 )
 
+type ColorApiResponse struct {
+	Hex  map[string]string `json:"hex"`
+	Name map[string]string `json:"name"`
+	Rgb  map[string]string `json:"rgb"`
+}
+
 func New() HelixThemeConfig {
 	return HelixThemeConfig{}
+}
+
+func MakeColorPalette(colorCodes []string, theme theme.Theme) {
+	log.Printf("Generating Color Palette")
+
+	var colorApiResponse []ColorApiResponse
+
+	// for _, v := range theme.Colors {
+	// 	parts := strings.Split(v, "#")
+	// 	resp, err := http.Get("https://www.thecolorapi.com/id?" + parts[1])
+
+	// 	error.HandleError(err)
+
+	// 	err = json.Unmarshal([]byte(resp), &colorApiResponse)
+
+	// 	error.HandleError(err)
+
+	// 	log.Printf("%v", resp)
+
+	// }
+
+	badge := strings.Split(theme.Colors["badge.background"], "#")
+
+	resp, err := http.Get("https://www.thecolorapi.com/id?hex=" + badge[1])
+
+	error.HandleError(err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	error.HandleError(err)
+
+	err = json.Unmarshal(body, &colorApiResponse)
+
+	log.Printf("%v", colorApiResponse)
 }
 
 func FromVsConfig(theme.Theme) {
--- convert/convert.go
+++ convert/convert.go
@@ -3,6 +3,7 @@ package convert
 import (
 	"log"
 
+	"github.com/Inalegwu/babel/adapters/helix"
 	"github.com/Inalegwu/babel/theme"
 )
 
@@ -35,11 +36,14 @@ func (c *Converter) toHelixConfig(theme theme.Theme) {
 	log.Printf("Converting %s to Helix Theme Format", theme.Name)
 	log.Printf("Theme type is %s", theme.Theme_type)
 	var colorCodes ColorCodes
-	// var palette helix.Palette
 
 	for _, t := range theme.Colors {
 		colorCodes = append(colorCodes, t)
 	}
 
 	log.Printf("Found %v Color Codes", len(colorCodes))
+
+	helix.MakeColorPalette(colorCodes, theme)
+
+	log.Printf("Color Palette Generated")
 }
